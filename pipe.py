3313207b513a471430131dbb6a4ec26a9661bd2c
mfitzasp <mfitzasp@gmail.com>
2024-02-19 19:24:14 +1100

    Update pipe.py

--- pipe.py
+++ pipe.py
@@ -5,3 +5,42 @@ Created on Mon Feb 19 17:07:31 2024
 @author: psyfi
 """
 
+
+# Queue of processing
+# Create queue of files for processing
+
+
+
+
+# Array of completed tokens
+
+
+
+# Simple watcher version
+
+while True:
+    
+    
+    # Check resources aren't being hammered
+    
+    
+    # Check there is new stuff in the local directory
+    
+        # If so, chuck them in the queue and keep track of the completed tokens
+        # Queue items: list of filenames, links to calibration frames, telescope, cameraname, filterlist, mono, aropipe
+        
+        
+    
+    
+    
+
+
+    # at some point during the day, late afternoon perhaps, pause processing to get the latest calibration files
+    print ("yay")
+
+
+# Nightly reset
+def nightly_reset(self):
+    
+    # Basically downloads and re-loads the latest calibration files
+    print ("yay")
