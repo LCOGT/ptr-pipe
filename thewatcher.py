ced5c48f6f8389c3f771f512ee6375e5f7fb6565
Michael Fitzgerald <mfitzasp@gmail.com>
2025-06-09 00:03:25 +0000

    making config path absolute

--- thewatcher.py
+++ thewatcher.py
@@ -441,7 +441,10 @@ def main():
 
     # site_name=files[0].split('_')[-1]
 
-    with open('config.json', 'r') as f:
+    script_dir = os.path.dirname(os.path.abspath(__file__))  
+    config_path = os.path.join(script_dir, 'config.json')
+    
+    with open(config_path, 'r') as f:
         config = json.load(f)
     
     pipe_id = config['pipe_id'] 
